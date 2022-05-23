702254b96e469ec1e31d50b6f0476f2b9bb029d1
Sammy Muchiri <dev.miano@gmail.com>
2022-05-23 23:14:39 +0300

    created file and folder structure

---
+++ src/components/charts/FinChart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FinChart = () => {
+  return (
+    <div>FinChart</div>
+  )
+}
+
+export default FinChart
---
+++ src/components/index.jsx
@@ -0,0 +1,23 @@
+// Main components
+export { default as Button } from './Button';
+export { default as Cart } from './Cart';
+export { default as ChartHeader } from './ChartHeader';
+export { default as Chat } from './Chat';
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
+export { default as Navbar } from './Navbar';
+export { default as Notification } from './Notification';
+export { default as Sidebar } from './Sidebar';
+export { default as ThemeSettings } from './ThemeSettings';
+export { default as UserProfile } from './UserProfile';
+
+// Chart components
+export { default as AreaChart } from './charts/AreaChart';
+export { default as BarChart } from './charts/BarChart';
+export { default as ColorMapChart } from './charts/ColorMapChart';
+export { default as FinChart } from './charts/FinChart';
+export { default as LineChart } from './charts/LineChart';
+export { default as PieChart } from './charts/PieChart';
+export { default as PyramidChart } from './charts/PyramidChart';
+export { default as SparkLineChart } from './charts/SparkLineChart';
+export { default as StackedChart } from './charts/StackedChart';
